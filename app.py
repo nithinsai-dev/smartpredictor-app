0971ac3882600ce9676914dd49eb9556bc6194c4
student-nithin <ninsai2007@gmail.com>
2026-03-19 14:17:47 +0530

    2nd model done

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask,render_template,request,jsonify
 import pickle
 import numpy as np
-from pyexpat import features
 
 app = Flask(__name__)
-
 seeds_model = pickle.load(open('models/seeds_model.pkl','rb'))
+crop_model = pickle.load(open('models/crop_model.pkl','rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods = ['POST'])
-def predict():
+@app.route('/seeds')
+def seeds():
+    return render_template('seeds.html')
+
+@app.route('/crop')
+def crop():
+    return render_template('crop.html')
+
+@app.route('/predict_seeds',methods = ['POST'])
+def predict_seeds():
     features = [
         float(request.form['A']),
         float(request.form['P']),
@@ -30,6 +37,22 @@ def predict():
     result = varieties[prediction]
 
     return jsonify({'prediction':result})
+
+@app.route('/predict_crop',methods = ['POST'])
+def predict_crop():
+    features = [
+        float(request.form['N']),
+        float(request.form['p']),
+        float(request.form['K']),
+        float(request.form['temperature']),
+        float(request.form['humidity']),
+        float(request.form['pH']),
+        float(request.form['rainfall'])
+    ]
+
+    features_array = np.array([features])
+    prediction = crop_model.predict(features_array)[0]
+    return jsonify({'prediction',str(prediction)})
 
 if __name__ == '__main__':
     app.run(debug=True)
